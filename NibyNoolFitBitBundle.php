a0acc2dc2b903a457544b977d67fcc1f3d692490
Michael Lambert <michael@alphageek.com.au>
2014-04-01 22:11:40 +1100

    Created Bundle file

---
+++ NibyNoolFitBitBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace NibyNool\FitBitBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * NibyNoolFitBitBundle
+ *
+ * @author Michael Lambert <michael2alphageek.com.au>
+ * @package NibyNool\FitBitBundle
+ */
+class NibyNoolFitBitBundle extends Bundle
+{
+}
