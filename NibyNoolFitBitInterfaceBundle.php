ed77df423923447d8964569e7385424bf9d19ee0
Michael Lambert <michael@alphageek.com.au>
2014-04-21 17:24:23 +1000

    Created Bundle

---
+++ NibyNoolFitBitInterfaceBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace NibyNool\FitBitInterfaceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class NibyNoolFitBitInterfaceBundle
+ *
+ * @package NibyNool\FitBitInterfaceBundle
+ */
+class NibyNoolFitBitInterfaceBundle extends Bundle
+{
+}
