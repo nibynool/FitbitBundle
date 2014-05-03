4ce8fc760313e0df5e72b6ded94360c8aa990372
Michael Lambert <michael@alphageek.com.au>
2014-05-03 21:38:02 +1000

    Added ability to select storage provider

--- Fitbit/TokenStorage.php
+++ Fitbit/TokenStorage.php
@@ -4,6 +4,7 @@ namespace NibyNool\FitBitBundle\FitBit;
 
 use OAuth\OAuth1\Token\StdOAuth1Token;
 use OAuth\Common\Storage\Memory;
+use OAuth\Common\Storage\Session;
 
 class TokenStorage
 {
@@ -15,22 +16,23 @@ class TokenStorage
 	/**
 	 * Constructor for the token storage
 	 *
+	 * @param string      $storage The storage to use for the token
 	 * @param string|null $token  The token to be added to the storage if this is pre-authorised
 	 * @param string|null $secret The secret associated with the token.
 	 */
-	public function __construct($token = null, $secret = null)
+	public function __construct($storage = 'memory', $token = null, $secret = null)
 	{
 		$this->token = new StdOAuth1Token();
+		if ($storage == 'memory') $this->adapter = new Memory();
+		elseif ($storage == 'session') $this->adapter = new Session();
 		if ($token !== null && $secret !== null)
 		{
 			$this->token->setRequestToken($token);
 			$this->token->setRequestTokenSecret($secret);
 			$this->token->setAccessToken($token);
 			$this->token->setAccessTokenSecret($secret);
+			$this->adapter->storeAccessToken('FitBit', $this->token);
 		}
-
-		$this->adapter = new Memory();
-		$this->adapter->storeAccessToken('FitBit', $this->token);
 	}
 
 	/**
