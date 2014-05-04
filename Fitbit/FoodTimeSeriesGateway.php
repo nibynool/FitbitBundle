1bd10b8bd1c76304f47a4abe7560568aacea07bc
Michael Lambert <michael@alphageek.com.au>
2014-05-04 18:27:18 +1000

    Added PHPDoc definitions for calls to the food time series gateway

--- Fitbit/FoodTimeSeriesGateway.php
+++ Fitbit/FoodTimeSeriesGateway.php
@@ -2,6 +2,13 @@
 
 namespace NibyNool\FitBitBundle\FitBit;
 
+/**
+ * Class FoodTimeSeriesGateway
+ *
+ * @package NibyNool\FitBitBundle\FitBit
+ * @method array getCaloriesIn(\DateTime $baseDate, string $period, \DateTime $endDate)
+ * @method array getWater(\DateTime $baseDate, string $period, \DateTime $endDate)
+ */
 class FoodTimeSeriesGateway extends TimeSeriesEndpointGateway {
     /**
      * base fragment for this resources uri
