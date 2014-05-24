a9a0285c5616a0e430efbdcdb96bb33fee7856fb
Michael Lambert <michael@alphageek.com.au>
2014-05-24 23:30:01 +1000

    Fixed Typo in PHPDoc block

--- NibyNoolFitBitBundle.php
+++ NibyNoolFitBitBundle.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 /**
  * NibyNoolFitBitBundle
  *
- * @author Michael Lambert <michael2alphageek.com.au>
+ * @author Michael Lambert <michael@alphageek.com.au>
  * @package NibyNool\FitBitBundle
  */
 class NibyNoolFitBitBundle extends Bundle
